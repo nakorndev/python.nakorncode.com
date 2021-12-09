ab8e3f693c5b510264c33f41a49b96823dcdaf15
Nakorn Sinpadung <nakorndev@gmail.com>
2021-12-09 13:16:28 +0700

    Change dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'docs'),
     clean: true,
   },
   plugins: [
